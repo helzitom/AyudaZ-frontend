08d36e56cca6a2e14856fd007e757b5a48269a7f
Helsito <U23221074@utp.edu.pe>
2026-05-14 02:39:37 -0500

    todo

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,17 @@
 import { initializeApp } from 'firebase/app';
-import {
-  getAuth,
-  GoogleAuthProvider,
-  FacebookAuthProvider
-} from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBFm5LS4COo44ozF_AYzdU3WzG77K1v130",
-  authDomain: "ayuda-z-c22bb.firebaseapp.com",
-  projectId: "ayuda-z-c22bb",
-  storageBucket: "ayuda-z-c22bb.firebasestorage.app",
-  messagingSenderId: "400356346915",
-  appId: "1:400356346915:web:1686e193926ecba6c67596",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
-console.log("API KEY:", firebaseConfig.apiKey);
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
 export const googleProvider = new GoogleAuthProvider();
 export const facebookProvider = new FacebookAuthProvider();
